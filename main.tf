2288959aee5345fd10fd0135a7c7761aadbbd5ae
calvoeduer <calvoeduer@gmail.com>
2022-11-09 19:09:43 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "task31" {
+    ami = "ami-09d3b3274b6c5d4aa"
+    instance_type = "t2.micro"
+
+    tags = {
+        "Env" = "Dev"
+    }
+}
+
